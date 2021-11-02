5b32ce0f4595c2fe643af61b6a33a4ae25cbf8bc
RemiArata <50224993+RemiArata@users.noreply.github.com>
2021-11-02 17:50:00 -0600

    Made minor fixes, improved how it deals with incoming data, and now returns JSON object

--- SimpleAPI/basic_api.py
+++ SimpleAPI/basic_api.py
@@ -1,22 +1,25 @@
 
 import hashlib
 import hmac
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def generate_HMAC_token():
-    passed_in_message = request.get_data()
-    stripped_message = f"{passed_in_message[3:]}"
+    data = request.form.get('id')
     key = b'0f8f13f4368f11ec8d3d0242ac130003'
 
     signiture = hmac.new(
         key,
-        str.encode(stripped_message),
+        str.encode(data),
         hashlib.sha256
     ).hexdigest()
-    return passed_in_message.decode('utf-8') + f"&Signature={signiture}"
+
+    payload = {"id": f"{data}", "signiture": signiture}
+    return jsonify(payload)
+
+
 
 if __name__ == "__main__":
     app.run()
